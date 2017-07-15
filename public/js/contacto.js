841c7418ead9908f6cc5e885701529a61af146e2
MSlinky <wwwmario15@hotmail.com>
2017-07-15 03:37:55 -0500

    Add files via upload

--- public/js/contacto.js
+++ public/js/contacto.js
@@ -2,11 +2,18 @@ var mexicoMap = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m
 var spain = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33139.2656163936!2d-1.6857941027984913!3d42.76456307887342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5091f1d7d0b7b7%3A0x9d887953ee1f6592!2sPol.+Ind.+Comarca+II-F%2C+12%2C+31191+Barbat%C3%A1in%2C+Navarra%2C+Espa%C3%B1a!5e0!3m2!1ses-419!2smx!4v1496831982817" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>';
 
 function info(url){
-	if('/'+url[1] =="/Contacto"){
-		$('.textBanner').html("Contáctanos");
-		$('.banner').css({
-			"background-image": "url(/public/imgs/banners/Portada_contacto.jpg)"
-		});
+	$('.banner').css({
+				"background-image": "url(/public/imgs/banners/Portada_contacto.jpg)"
+			});
+	if(ObjUrl.getUrl()[0] == 'es'){
+		if('/'+url[1] =="/Contacto"){
+			$('.textBanner').html("Contáctanos");
+			
+		}
+	}else{
+		if('/'+url[1] =="/Contacto"){
+			$('.textBanner').html("Contact us");
+		}
 	}
 }
 
@@ -52,7 +59,45 @@ function content (data){
 			}
 		});
 	}else{
-		
+		let content = '';
+			content +='<div class="textoSection textoSectionCompleto">'+
+	        	'<center><h4>Send us your messages </h4><center>'+
+	        '</div>'+
+
+	        '<div class="formulario">'+
+	        	'<form id="comentarios">'+
+					'<div id="mensaje"></div>'+
+					'<div id="mensaje_error"></div>'+
+					'<label >Name</label>'+
+					'<input class="efectSombra" name="nombre" type="text">'+
+					'<label >Email</label>'+
+					'<input class="efectSombra" name="correo" id="correo" type="text">'+
+					'<label >Subject</label>'+
+					'<input class="efectSombra" name="asunto" type="text">'+
+					'<label >Message</label>'+
+					'<textarea class="efectSombra" name="comentario"></textarea>'+
+					'<center><input name="accion" class="boton" type="submit" value="Send"></center>'+
+				'</form>'+
+	        '</div><div class="mapa">'+
+
+	        	'<div><span class="map boton">Mexico</span><span class="map boton">Spain</span></div>'+
+				'<br>'+
+				'<br>'+
+				'<div class="divMap">'+
+					
+				'</div>'+
+	        '</div>';
+	    $('#contacto').html(content);
+
+	    $('.divMap').html(mexicoMap);
+
+	    $('.map').click((event)=>{
+			if($(event.target)[0]['textContent'] == "México"){
+				$('.divMap').html(mexicoMap);
+			}else{
+				$('.divMap').html(spain);
+			}
+		});
 	}
 }
 
--- public/js/plantillas.js
+++ public/js/plantillas.js
@@ -92,7 +92,7 @@ function info(url){
 				});
 
 				$('.textoSection').html(
-					'<h3>SOLDADOR DE ULTRASONIDO</h3>'+
+					'<h3>SOLDADOR DE ULTRASONIDO</h3></br>'+
 					'<div>'+
 						'<p>La soldadura Ultrasónica es un proceso de soldadura en estado sólido que suelda por la aplicación vibraciones de energía de alta frecuencia mientras las piezas están siendo sujetadas a presión.</p>'+
 						'<p>Nuestro ultrasonido ofrece alta calidad en soldadura, uniforme y resistente. No hay calentamiento por lo que no se fragilizan zonas afectadas por calor. Los tiempos de ciclo son menores a un segundo. La contaminación superficial y oxidación no afectan. No contamina al no emitir gases ni residuos.</p>'+
@@ -129,25 +129,25 @@ function info(url){
 		} 
 	}else{
 		if(url[1] == "Nosotros"){
-			$('.textBanner').html("La mejor tecnología europea");
+			$('.textBanner').html("Best European Technology");
 			$('.banner').css({
 				"background-image": "url(/public/imgs/banners/portada_nosotros.jpg)"
 			});
 		}else{
 			if(url[2] =="BandaGallinaza"){
-				$('.textBanner').html("Poultry belt");
+				$('.textBanner').html("Poultry Belt");
 				$('.banner').css({
 					"background-image": "url(/public/imgs/banners/portada_banda_gallinaza.jpg)"
 				});
 
 				$('.textoSection').html(
-					'<h3>BANDA DE GALLINAZA</h3>'+
-					'<p>Nuestra banda de transporte y expulsión de gallinaza está fabricada con un copolímero de impacto, el cual da mayor resistencia y longevidad al producto.</p>'+
+					'<h3>POULTRY BELT</h3>'+
+					'<p>Our conveyor and poultry expulsion belt is manufactured with an impact copolymer, which gives greater resistance and longevity to the product.</p>'+
 					'<ul>'+
-						'<li>Material: Polipropileno (LYONDELLBASSELL MOPLEN EP440G) Materia Prima Virgen.</li>'+
-						'<li>Color: Blanco y Negro. (Con costo adicional)</li>'+
-						'<li>Medidas: Fabricamos a medida todos los anchos (85 cm – 250 cm) y espesores (0.8 mm – 1.5 mm).</li>'+
-						'<li>Tratamiento UV.</li>'+
+						'<li>Material: Polypropylene (LYONDELLBASSELL MOPLEN EP440G) Pure raw materia.</li>'+
+						'<li>Color: Black and White. (With additional cost).</li>'+
+						'<li>Measures: We manufacture all widths (85 cm - 250 cm) and thicknesses (0.8 mm - 1.5 mm).</li>'+
+						'<li>UV treatment.</li>'+
 					'</ul>'
 				);
 
@@ -163,12 +163,12 @@ function info(url){
 						'<div class="fichaTecnica"><span><img src="/public/imgs/Made-Spain.png" alt="">'+descarga+'</span></div>'+
 					'</div>');
 			}else if(url[2] =="BandaHuevo"){
-				$('.textBanner').html("Banda de Huevo");
+				$('.textBanner').html("Egg Band");
 				$('.banner').css({
 					"background-image": "url(/public/imgs/banners/portada_banda_huevo.png)"
 				});
 
-				$('.textoSection').html('<h3>BANDA DE HUEVO</h3><p>Nuestra banda de recolección de huevo está diseñada para ofrecer resistencia, poseen un tejido que garantiza absorción de huevos rotos, y evitar suciedad en la demás producción.</p><ul><li>Material: Hilo de Polipropileno. Y estabilizado para rayos UV.</li><li>Color: Beige, con rayas laterales azules.</li><li>Medidas: Anchos desde 80 mm a 200 mm</li><li>Resistentes a hongos y bacterias.</li><li>Lavables, no mantienen la humedad.</li></ul>');
+				$('.textoSection').html('<h3>EGG BAND</h3><p>Our egg collection band is designed to offer resistance, they have a tissue that guarantees absorption of broken eggs, and to avoid dirt in the other production.</p><ul><li>Material: Polypropylene thread. And stabilized for UV rays.</li><li>Color: Beige, with blue side stripes.</li><li>Measures: Widths from 80 mm to 200 mm</li><li>Resistant to fungi and bacterias.</li><li>Washable, do not hold moisture.</li></ul>');
 
 				let descarga = '<a href="/public/imgs/Bandas/BandaHuevo/bandas_avicolas_fichas_banda_transportadora_huevo_240517.pdf" download="BandadeHuevo.pdf"><img src="/public/imgs/descarga-ficha.png" alt=""></a>';
 
@@ -179,26 +179,26 @@ function info(url){
 					"background-image": "url(/public/imgs/banners/portada_deflectordegallinaza.jpg)"
 				});
 
-				$('.textoSection').html('<h3>DEFLECTOR DE GALLINAZA</h3><p>Nuestro Deflector está fabricado con un copolimero de polipropileno de alta tenacidad.</p><ul><li>Material: Polipropileno (LYONDELLBASSELL MOPLEN EP440G) Materia Prima Virgen.</li><li>Medidas: Fabricamos a medida todos los anchos (60 cm) y espesores (0.8 mm). Tenemos también pedidos a medida para todas las jaulas del mercado.</li><li>Se puede fabricar con o sin ojillos de fijación.</li><li>Tratamiento UV.</li><li>Color: Blanco y Negro. (Con costo adicional)</li></ul>');
+				$('.textoSection').html('<h3>POULTRY DEFLECTOR</h3><p>Our Deflector is manufactured with a high tenacity polypropylene copolymer.</p><ul><li>Material: Polypropylene (LYONDELLBASSELL MOPLEN EP440G) Pure raw materia.</li><li>Measures: We manufacture all widths (60 cm) and thicknesses (0.8 mm). We also have custom orders for all cages in the market.</li><li>It can be made with or without fixing eyes.</li><li>UV treatment.</li><li>Color: Black and White. (With additional cost)</li></ul>');
 
 				let descarga = '<a href="/public/imgs/Bandas/DeflectorGallinaza/bandas_avicolas_fichas_deflector_gallinaza_240517.pdf" download="DeflectorGallinaza.pdf"><img src="/public/imgs/descarga-ficha.png" alt=""></a>';
 
 				$('.imagenBanderas').html('<div><div><img src="/public/imgs/Bandas/DeflectorGallinaza/foto_deflector.jpg" alt=""></div><div><img src="/public/imgs/Bandas/DeflectorGallinaza/foto_deflector2.jpg" alt=""></div></div><div><div><img src="/public/imgs/Bandas/DeflectorGallinaza/Copia de gráfica_banda_gallinaza.jpg" alt=""></div><div class="fichaTecnica"><span><img src="/public/imgs/Made-Spain.png" alt="">'+descarga+'</span></div></div>');
 			}else if(url[2] =="BandaElevadoraGallinaza"){
-				$('.textBanner').html("Banda Transversal Elevadora de Gallinaza");
+				$('.textBanner').html("POULTRY LIFT CROSS BAND");
 				$('.banner').css({
 					"background-image": "url(/public/imgs/banners/portada_banda_transversal.jpg)"
 				});
 
 				$('.textoSection').html(
-					'<h3>Banda Transversal Elevadora de Gallinaza</h3>'+
-					'<p>Nuestra banda transversal está fabricada con la mejor tecnología ofreciendo así alta resistencia. Vulcanizada.</p>'+
+					'<h3>Poultry Lift Cross Band</h3>'+
+					'<p>Our transverse band is manufactured with the best technology offering high resistance. Vulcanized.</p>'+
 					'<ul>'+
-						'<li>Material: Poliéster Rígido</li>'+
-						'<li>Peso: 3.50 kg /m2</li>'+
-						'<li>Color: Verde (cobertura superior) y Negro o crudo (cobertura interterior)</li>'+
-						'<li>Espesor Total: 2.90 mm (2 telas de 1.50 mm aprox)</li>'+
-						'<li>Antiestática.</li>'+
+						'<li>Material: Rigid Polyester</li>'+
+						'<li>Weight: 3.50 kg / m2</li>'+
+						'<li>Color: Green (top cover) and Black or Crude (inside cover)</li>'+
+						'<li>Total Thickness: 2.90 mm (2 screens of 1.50 mm aprox)</li>'+
+						'<li>Antistatic.</li>'+
 					'</ul>'
 				);
 
@@ -214,27 +214,27 @@ function info(url){
 						'<div></div>'+
 					'</div>');
 			}else if(url[2] =="SoldadorUltrasonido"){
-				$('.textBanner').html("SOLDADOR DE ULTRASONIDO");
+				$('.textBanner').html("ULTRASOUND WELDER");
 				$('.banner').css({
 					"background-image": "url(/public/imgs/banners/portada_soldador_ultrasonido.jpg)"
 				});
 
 				$('.textoSection').html(
-					'<h3>SOLDADOR DE ULTRASONIDO</h3>'+
+					'<h3>Ultrasound Welder</h3></br>'+
 					'<div>'+
-						'<p>La soldadura Ultrasónica es un proceso de soldadura en estado sólido que suelda por la aplicación vibraciones de energía de alta frecuencia mientras las piezas están siendo sujetadas a presión.</p>'+
-						'<p>Nuestro ultrasonido ofrece alta calidad en soldadura, uniforme y resistente. No hay calentamiento por lo que no se fragilizan zonas afectadas por calor. Los tiempos de ciclo son menores a un segundo. La contaminación superficial y oxidación no afectan. No contamina al no emitir gases ni residuos.</p>'+
+						'<p>Ultrasonic welding is a solid state welding process that welds high frequency energy vibrations by applying high pressure parts while the parts are being clamped.</p>'+
+						'<p>Our ultrasonic offers high quality in welding, uniform and resistant. There is no heating so it does not weaken areas affected by heat. Cycle times are less than one second. Surface contamination and oxidation do not affect. It does not pollute by not emitting gases or residues.</p>'+
 					'</div>'+
 					'<div style="padding-left: 100px">'+
 						'<ul>'+
-							'<li>Frecuencia: 40KHz</li>'+
-							'<li>Potencia: 250 Watt</li>'+
-							'<li>Temporizador Electrónico</li>'+
-							'<li>Filtro de red.</li>'+
-							'<li>Cable de conexión 5 mts</li>'+
-							'<li>Microamperómetro digital</li>'+
-							'<li>Dimensiones: 400mm x 330mm x135mm</li>'+
-							'<li>Peso total del equipo con maletín: 12 kg</li>'+
+							'<li>Frequency: 40KHz</li>'+
+							'<li>Power: 250 Watts</li>'+
+							'<li>Electronic Timer </li>'+
+							'<li>Network filter</li>'+
+							'<li>5m Connection cable</li>'+
+							'<li>Digital microamperometer</li>'+
+							'<li>Dimensions: 400mm x 330mm x 135mm</li>'+
+							'<li>Total weight of equipment with carrying case: 12kg</li>'+
 						'</ul>'+
 					'</div>'
 				);
@@ -259,7 +259,13 @@ function info(url){
 }
 
 function content(data){
-	info(ObjUrl.getUrl());
+	if(ObjUrl.getUrl()[1] == "Nosotros"){
+		$('#contentText').html(data['text']);
+		info(ObjUrl.getUrl());
+	}else{
+		info(ObjUrl.getUrl());
+	}
+	
 }
 
 $( document ).ready(function() {
